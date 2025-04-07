48065f8919afecbda92d8cd2eb00784ada1f5b1c
asafdomovich <asafdomo@gmail.com>
2025-04-07 19:35:29 +0600

    create movie controller

---
+++ src/main/java/com/att/tdp/popcorn_palace/controller/MovieController.java
@@ -0,0 +1,49 @@
+package com.att.tdp.popcorn_palace.controller;
+
+import com.att.tdp.popcorn_palace.dto.request.CreateMovieRequest;
+import com.att.tdp.popcorn_palace.dto.response.MovieResponse;
+import com.att.tdp.popcorn_palace.service.MovieService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService;
+
+    // Get all movies
+    @GetMapping("/all")
+    public ResponseEntity<List<MovieResponse>> getAllMovies() {
+        List<MovieResponse> movieResponses = movieService.getAllMovies();
+        return new ResponseEntity<>(movieResponses, HttpStatus.OK);
+    }
+
+    // Add a new movie
+    @PostMapping
+    public ResponseEntity<MovieResponse> addMovie(@Valid @RequestBody CreateMovieRequest createMovieRequest) {
+        MovieResponse movieResponse = movieService.addMovie(createMovieRequest);
+        return new ResponseEntity<>(movieResponse, HttpStatus.OK);
+    }
+
+    // Update a movie
+    @PostMapping("/update/{movieTitle}")
+    public ResponseEntity<Void> updateMovie(@PathVariable String movieTitle, @Valid @RequestBody CreateMovieRequest updateMovieRequest) {
+        movieService.updateMovie(movieTitle, updateMovieRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // Delete a movie
+    @DeleteMapping("/{movieTitle}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable String movieTitle) {
+        movieService.deleteMovie(movieTitle);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
